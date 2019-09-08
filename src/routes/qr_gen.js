f857b882ac237ef2a99244b66ed8ab7ce76d98bd
aldermann <vietanisme@gmail.com>
2019-09-08 15:34:23 +0700

    layout fix

--- src/routes/qr_gen.js
+++ src/routes/qr_gen.js
@@ -8,7 +8,8 @@ router.get("/pizza", async (req, res) => {
     const url = `${process.env.URL}/hook/pizza`;
     const s = `https://qr.id.vin/hook?url=${encodeURI(url)}&method=GET`;
     const image = await QR.toDataURL(s);
-    res.send(`<img src='${image}' alt="test"/>`);
+    // res.send(`<img src='${image}' alt="test"/>`);
+    res.render("pages/pizza_qr", { image, user: req.user });
 });
 
 router.get("/book", async (req, res) => {
@@ -17,10 +18,10 @@ router.get("/book", async (req, res) => {
         bookList.map(async book => {
             const url = `${process.env.URL}/hook/book?book_id=${book.book_id}`;
             const s = `https://qr.id.vin/hook?url=${encodeURI(url)}&method=GET`;
-            return QR.toDataURL(s);
+            return { image: await QR.toDataURL(s), book };
         }),
     );
-    res.render("pages/qr_gen", { codes: qrImages, user: req.user });
+    res.render("pages/book_qr", { codes: qrImages, user: req.user });
 });
 
 export default router;
